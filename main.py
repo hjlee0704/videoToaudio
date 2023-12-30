4f64f4f5a45c0b5e8a03a914fe8ade24adf70d5c
Anthony Lee <ahjlee74@gmail.com>
2023-12-29 22:34:44 -0800

    add user model

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
+class User(BaseModel):
+    username: str
+    email: str or None = None
+    full_name: str or None = None
+    disabled: bool or None = None
+    
 app = FastAPI()
+
+@app.post("/create/")
+async def  create():
+  return {}
 
 @app.get("/test/")
 async def test():
